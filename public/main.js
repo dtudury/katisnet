f89cb9a4da2bc297a8e111a0f95e60ed7069aed7
David Tudury <david.tudury@gmail.com>
2019-06-11 23:21:08 -0700

    damped

--- public/main.js
+++ public/main.js
@@ -4,62 +4,53 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 class Ball {
-  constructor () {
-    this.x = canvas.width * Math.random()
-    this.y = canvas.height * Math.random()
+  constructor (i) {
+    this.i = i
+    this.x = (canvas.width * Math.random() + canvas.width) / 3
+    this.y = (canvas.height * Math.random() + canvas.height) / 3
     this.dx = 0
     this.dy = 0
     this.connections = []
   }
-  update1 () {
+  accelerateToward (x, y, p, f) {
+    const dx = this.x - x
+    const dy = this.y - y
+    const d = dx * dx + dy * dy
+    this.dx += dx * Math.pow(d, p) * f
+    this.dy += dy * Math.pow(d, p) * f
+  }
+  updateVelocity () {
     balls.forEach(ball => {
       if (ball !== this) {
-        const dx = this.x - ball.x
-        const dy = this.y - ball.y
-        const d = dx * dx + dy * dy
-        this.dx += dx * Math.pow(d, -1) * 5
-        this.dy += dy * Math.pow(d, -1) * 5
+        this.accelerateToward(ball.x, ball.y, -1, 5)
       }
     })
     this.connections.forEach(ball => {
-      const dx = this.x - ball.x
-      const dy = this.y - ball.y
-      // const d = Math.sqrt(dx * dx + dy * dy)
-      // this.dx -= dx * 0.01
-      // this.dy -= dy * 0.01
-      const d = dx * dx + dy * dy
-      this.dx -= dx * Math.pow(d, 0.5) * 0.0001
-      this.dy -= dy * Math.pow(d, 0.5) * 0.0001
+      this.accelerateToward(ball.x, ball.y, 0.5, -0.0001)
     })
-    const cx = canvas.width / 2 - this.x
-    const cy = canvas.height / 2 - this.y
-    const c = Math.sqrt(cx * cx + cy * cy)
-    this.dx += cx * c * 0.00001
-    this.dy += cy * c * 0.00001
-    this.dx *= 0.99
-    this.dy *= 0.99
+    this.accelerateToward(canvas.width / 2, canvas.height / 2, 0.5, -0.00001)
+    this.dx *= 0.95
+    this.dy *= 0.95
   }
-  update2 () {
+  updatePosition () {
     this.x += this.dx
     this.y += this.dy
   }
 }
 
 const balls = []
-const connections = []
 for (let i = 0; i < 20; i++) {
-  balls.push(new Ball())
+  balls.push(new Ball(i))
 }
 for (let i = 0; i < 20; i++) {
   let pairs = new Set()
-  while (pairs.size < 1) {
+  while (pairs.size < 2) {
     let j = Math.floor(Math.random() * 20)
     if (j !== i && balls[j].connections.indexOf(balls[i]) === -1) {
       pairs.add(j)
     }
   }
   pairs.forEach(j => {
-    connections.push([balls[i], balls[j]])
     balls[i].connections.push(balls[j])
     balls[j].connections.push(balls[i])
   })
@@ -90,12 +81,44 @@ function draw () {
     canvas.height = window.innerHeight
   }
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  balls.forEach(ball => ball.update1())
-  balls.forEach(ball => ball.update2())
+  balls.forEach(ball => ball.updateVelocity())
+  balls.forEach(ball => ball.updatePosition())
   balls.forEach(ball => drawBall(ctx, ball))
+  const connections = []
+  balls.forEach(ball => {
+    ball.connections.forEach(connection => {
+      if (connection.i > ball.i) {
+        connections.push([ball, connection])
+      }
+    })
+  })
   connections.forEach(connection => drawConnection(ctx, connection[0], connection[1]))
   window.requestAnimationFrame(draw)
 }
+
+function improve () {
+  const sortedBalls = balls.sort((a, b) => a.connections.length - b.connections.length)
+  let ball
+  if (Math.random() > 0.5) {
+    ball = sortedBalls[sortedBalls.length - 1]
+    while (ball.connections.length > 3) {
+      const connection = ball.connections[Math.floor(Math.random() * ball.connections.length)]
+      ball.connections.splice(ball.connections.indexOf(connection), 1)
+      connection.connections.splice(connection.connections.indexOf(ball), 1)
+    }
+  } else {
+    ball = sortedBalls[0]
+    while (ball.connections.length < 3) {
+      const newConnection = balls[Math.floor(Math.random() * balls.length)]
+      if (newConnection !== ball && ball.connections.indexOf(newConnection) === -1) {
+        ball.connections.push(newConnection)
+        newConnection.connections.push(ball)
+      }
+    }
+  }
+}
+
+setInterval(improve, 1000)
 
 document.body.onresize = draw
 draw()
